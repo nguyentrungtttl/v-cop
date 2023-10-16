5075a334113a64c29bcc4b763f0a7acfb09426ba
Ken Lee <kenleweb13@gmail.com>
2023-10-16 17:24:16 +0700

    AuthDesign

---
+++ Src/Screens/Authority/Home/Evidence/components/style.js
@@ -0,0 +1,49 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    box:{
+        backgroundColor:"#d6d2d2",
+        borderRadius: 8,
+        marginHorizontal:"10%",
+        flexDirection:"row",
+        justifyContent:"space-around",
+        paddingVertical:"3%",
+        marginTop:"5%"
+    },
+    blue:{
+        color:"#1d3a70",
+        fontSize:16,
+        fontWeight:"bold"
+    },
+    grey:{
+        color:"#8391a1",
+        fontSize: 13
+    },
+    black:{
+        color:"black",
+        fontSize:14,
+        fontWeight:"bold"
+    },
+    white:{
+        color:"white",
+        fontWeight:"bold",
+        fontSize:14,
+        textAlign:"center",
+        top:"23%"
+    },
+    bluebtn:{
+        backgroundColor:"#143072",
+        borderWidth: 0.5,
+        borderColor:"black",
+        borderRadius: 8,
+        height: 30,
+        marginRight:"15%"
+    },
+    container:{
+        marginTop:"5%"
+    }
+
+
+})
+
+export {styles}
